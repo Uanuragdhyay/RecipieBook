c426a8eb4cd093273d994bdbead03b9728387800
Anurag <uanurag216@gmail.com>
2025-01-26 15:56:41 +0530

    initiated home page

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController searchController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +27,65 @@ class _HomeState extends State<Home> {
               )
             ),
           ),
-          SafeArea(child: Text("HEMLOO"),
-          )
+        Column(
+          children: [
+
+            //Search Bar
+            SafeArea(
+              child: Container(
+                //Search Wala Container
+
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24)),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        if((searchController.text).replaceAll(" ", "") == "")
+                        {
+                          print("Blank search");
+                        }else{
+                          Navigator.pushReplacementNamed(context, "/loading",arguments: {
+                            "searchText" : searchController.text,
+                          });
+                        }
+
+                      },
+                      child: Container(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.blueAccent,
+                        ),
+                        margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: "Let's Cook Something!"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("What you want to cook today ?",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.black),),
+                  SizedBox(height: 12,),
+                  Text("Let's cook something new !!" , style: TextStyle(fontSize: 25,color: Colors.grey.shade900),)
+                ],
+              ),
+            ),
+          ],
+        )
         ],
       )
     );
