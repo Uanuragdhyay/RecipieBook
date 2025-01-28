ec96faab246348e401a36949727f0ec965365235
Anurag <uanurag216@gmail.com>
2025-01-28 14:05:32 +0530

    created listview.builder  page

--- lib/home.dart
+++ lib/home.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
 class Home extends StatefulWidget {
-
-
   @override
   State<Home> createState() => _HomeState();
 }
@@ -16,20 +14,20 @@ class _HomeState extends State<Home> {
   List<RecipeModel> recipeList = <RecipeModel>[];
   TextEditingController searchController = new TextEditingController();
 
-  getRecipe( String query) async
-  {
-    String url = "https://api.edamam.com/search?q=$query&app_id=2eeffef2&app_key=a22a4be89733fe993fa6c5ad8ed8f0df&from=0&to=3&calories=591-722&health=alcohol-free";
+  getRecipe(String query) async {
+    String url =
+        "https://api.edamam.com/search?q=$query&app_id=2eeffef2&app_key=a22a4be89733fe993fa6c5ad8ed8f0df&from=0&to=3&calories=591-722&health=alcohol-free";
     Response response = await get(Uri.parse(url));
-    Map data= jsonDecode(response.body);
-   // log(data.toString());
+    Map data = jsonDecode(response.body);
+    // log(data.toString());
 
-    data["hits"].forEach((element){
+    data["hits"].forEach((element) {
       RecipeModel recipeModel = new RecipeModel();
       recipeModel = RecipeModel.fromMap(element['recipe']);
       recipeList.add(recipeModel);
       log(recipeList.toString());
     });
-    recipeList.forEach((Recipe){
+    recipeList.forEach((Recipe) {
       print(Recipe.applabel);
       print(Recipe.Calories);
     });
@@ -45,79 +43,101 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height ,
-            decoration: BoxDecoration(
-              gradient:LinearGradient(
-                  colors:[
-                    Colors.white70,
-                    Colors.yellowAccent.shade100,
-                    Colors.orangeAccent.shade200
-                  ]
-              )
-            ),
-          ),
-        Column(
-          children: [
+        body: Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.white70,
+            Colors.yellowAccent.shade100,
+            Colors.orangeAccent.shade200
+          ])),
+        ),
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              //Search Bar
+              SafeArea(
+                child: Container(
+                  //Search Wala Container
 
-            //Search Bar
-            SafeArea(
-              child: Container(
-                //Search Wala Container
-
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(24)),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        if((searchController.text).replaceAll(" ", "") == "")
-                        {
-                          print("Blank search");
-                        }else{
-                          getRecipe(searchController.text);
-                        }
-                      },
-                      child: Container(
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.blueAccent,
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24)),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if ((searchController.text).replaceAll(" ", "") == "") {
+                            print("Blank search");
+                          } else {
+                            getRecipe(searchController.text);
+                          }
+                        },
+                        child: Container(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                         ),
-                        margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                       ),
+                      Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Let's Cook Something!"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "What you want to cook today ?",
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black),
                     ),
-                    Expanded(
-                      child: TextField(
-                        controller: searchController,
-                        decoration: InputDecoration(
-                            border: InputBorder.none, hintText: "Let's Cook Something!"),
-                      ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      "Let's cook something new !!",
+                      style: TextStyle(fontSize: 25, color: Colors.grey.shade900),
                     )
                   ],
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("What you want to cook today ?",style: TextStyle(fontSize: 40,fontWeight: FontWeight.w800,color: Colors.black),),
-                  SizedBox(height: 12,),
-                  Text("Let's cook something new !!" , style: TextStyle(fontSize: 25,color: Colors.grey.shade900),)
-                ],
-              ),
-            ),
-          ],
+              Container(
+                child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                    itemCount: 1000,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return AnuuText();
+                    }),
+              )
+            ],
+          ),
         )
-        ],
-      )
+      ],
+    ),
     );
   }
+}
+
+Widget AnuuText() {
+  return Text("Anuu is A good Boy");
 }
